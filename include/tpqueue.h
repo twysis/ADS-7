73ee1aee09aa36a214c35ff718bbe4e15d00b955
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-04-30 17:29:20 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,7 +1,7 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-#include <string>>
+#include <string>
 
 template<typename T>
 class TPQueue {
@@ -65,7 +65,7 @@ void TPQueue<T>:: push(const T& value) {
       temp->previous = item;
     }
 }
-};
+
 
 struct SYM {
   char ch;
