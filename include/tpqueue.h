b586bce306e2528d523ea2eed56defdd5e328ae6
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-04-30 17:22:43 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,66 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+  private:
+  struct ITEM {
+    T value;
+    ITEM* next, * previous;
+  };
+  ITEM* start, * end;
+  TPQueue::ITEM* create(const T& value) {
+    ITEM* item = new ITEM;
+    item->value = value;
+    item->next = item->previous = nullptr;
+    return item;
+}
+ public:
+  TPQueue() : start(nullptr), end(nullptr) {} 
+  T pop();
+  void push(const T& value);
+};
+
+template<typename T>
+T TPQueue<T>:: pop() {
+  if (start && end) {
+    ITEM* temp = start->next;
+    if (temp) {
+      temp->previous = nullptr;
+    }
+    T value = start->value;
+    delete start;
+    start = temp;
+    if (!start) {
+      end = nullptr;
+    }
+  return value;
+  } else {
+    throw std::string("Queue empty");
+  }
+}
+
+template<typename T>
+void TPQueue<T>:: push(const T& value) {
+  ITEM* temp = start;
+  ITEM* item = create(value);
+  while (temp && temp->value.prior >= value.prior)
+    temp = temp->next;
+    if (!temp && start) {
+      end->next = item;
+      item->previous = end;
+      end = item;
+    } else if (!temp && !start) {
+      start = end = item;
+    } else if (!temp->previous) {
+      start->prev = item;
+      item->next = start;
+      start = item;
+    } else {
+      temp->previous->next = item;
+      item->previous = temp->previous;
+      item->next = temp;
+      temp->previous = item;
+    }
+}
 };
 
 struct SYM {
